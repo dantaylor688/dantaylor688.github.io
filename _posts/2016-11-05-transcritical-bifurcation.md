17e2114d89dd0bd5321574b81ab04f050ed664a9
dantaylor688 <dantaylor688@gmail.com>
2016-11-05 23:27:45 -0400

    transcritical post

---
+++ _posts/2016-11-05-transcritical-bifurcation.md
@@ -0,0 +1,79 @@
+---
+Transcritical Bifurcations
+---
+
+In my [last post](post_link), we discussed the simplest type of bifurcation, the saddle-node bifurcation. Next, we look at a different type of bifurcation. The transcritical bifurcation has the normal form
+
+$$
+\begin{equation}
+\dot{x} = rx - x^2
+\end{equation}
+$$
+
+In this type of bifurcation, instead of both fixed points annihilating each other, the fixed points "swap" stability. Let's look at the normal form (1) to see what this means.
+
+![plane]({{ site.url }}/assets/transcritical/phase.png "r varries")
+
+Notice that $x=0$ is a fixed point for a $r$.  For $r<0$, $x = 0$ is stable and $x = r$ is unstable.  For $r>0$ the fixed points "swap" stability and $x=0$ becomes unstable while $x=r$ is now stable. The bifurcation diagram is given next
+
+![plane]({{ site.url }}/assets/transcritical/bif.png "bifurcation")
+
+### Example ###
+Next, we consider the example 
+
+$$
+\begin{equation}
+\dot{x} = rx - \log(1+x)
+\end{equation}
+$$
+
+We will handle this in two different ways. First, we look at the phase diagram of (2) for several values of $r$.
+
+![plane]({{ site.url }}/assets/transcritical/log-bif.png "log phase diagram")
+
+Notice that $x=1$ is a fixed point for all $r$. We can employ the same method as in the [previous post](previous_post_link) and solve for $r$ in terms of $x$ and then just invert the axes. For a fixed point, 
+
+$$
+\begin{align*}
+0 &= rx-\log(1+x)\\
+r &= \frac{\log(1+x)}{x}
+\end{align*}
+$$
+
+When we invert the axes (making the plot of $x$ as a function of $r$) we get
+
+![plane]({{ site.url }}/assets/transcritical/log.png "x*")
+
+Another strategy is to make use of the Taylor expansion of $\log(1+x)$ about the point $x=1$.
+
+$$
+\log(1+x) = x - \frac{x^2}{2} + O(x^3)
+$$
+
+The the system (2) becomes
+
+$$
+\begin{align*}
+\dot{x} &= rx - x + \frac{x^2}{2} + O(x^3)\\
+&= (r-1)x + \frac{x^2}{2} + O(x^3)
+\end{align*}
+$$
+
+So 
+
+$$
+\begin{align}
+\dot{x} &\approx (r-1)x + \frac{x^2}{2}\\
+\implies \dot{x} &\approx -2 (r-1)x - x^2
+\end{align}
+$$
+
+Then again, the system has a trans-critical bifurcation at $r = 1$ and now a linear approximation of the bifurcation diagram (good for $x \approx 0$) is 
+
+![plane]({{ site.url }}/assets/transcritical/lin-approx.png "linear approximation")
+
+And here are the two plots together to get a better feel for how good the approximation is.
+
+![plane]({{ site.url }}/assets/transcritical/log-lin.png "comparison")
+
+We have yet to discuss, pitchfork bifurcations. It was my intention for that to be the topic of my next post. However, I recently started a new job working on the [autonomous car](http://media.gm.com/media/us/en/gm/news.detail.html/content/Pages/news/us/en/2016/mar/0311-cruise.html) at [GM](http://www.gm.com/index.html). Because of this, my area of focus will be shifting, so I don't know if my current study of nonlinear dynamics will continue nor do I know what the topic of my next post will be. We shall see.
---
+++ assets/bifurcation/transcritical.py
@@ -0,0 +1,195 @@
+from numpy import *
+from matplotlib import *
+from pylab import *
+import matplotlib.lines as mlines
+
+if __name__ == "__main__":
+    rc('text', usetex=True)
+    rc('font', family='serif')
+    fs = 20
+    ### Example 1
+    x = arange(-5,5,0.1)
+    
+
+    # r > 0
+    fig= figure(1)
+    ax = fig.add_subplot(311)
+    frame1 = plt.gca()
+    hold
+    r = 2
+    xdot = r*x - x**2
+    ax.plot(x,xdot,'b-')
+    ylim([min(xdot),max(xdot)+3])
+    xlim([min(x),max(x)])
+    ax.plot(x,zeros_like(x),'k-')
+    ax.plot(0,0,'bo',mfc='none',mec='b')
+    ax.plot(r,0,'bo',fillstyle='full',mec='b')
+    ax.set_title(r'$r > 0$')
+    frame1.axes.get_xaxis().set_visible(False)
+    frame1.axes.get_yaxis().set_visible(False)
+
+    ax.annotate('', xy=(-2, 0), xytext=(-1, 0),
+             arrowprops=dict(facecolor='black', shrink=0.04, width=1),
+             )
+    ax.annotate('', xy=(r-1, 0), xytext=(0.5, 0),
+             arrowprops=dict(facecolor='black', shrink=0.04, width=1),
+             )
+    ax.annotate('', xy=(r+1, 0), xytext=(r+2, 0),
+             arrowprops=dict(facecolor='black', shrink=0.04, width=1),
+             )
+
+    # lft = mlines.Line2D.fillStyles
+    # r = 0
+    ax = fig.add_subplot(312)
+    frame1 = plt.gca()
+    r = 0
+    xdot = r*x - x**2
+    ax.plot(x,xdot,'b-')
+    ax.plot(0,0,'bo',fillstyle='right',mec='b')
+    ylim([min(xdot),max(xdot)+3])
+    xlim([min(x),max(x)])
+    ax.plot(x,zeros_like(x),'k-')
+    ax.set_title(r'$r = 0$')
+    frame1.axes.get_xaxis().set_visible(False)
+    frame1.axes.get_yaxis().set_visible(False)
+
+    ax.annotate('', xy=(r-2, 0), xytext=(r-1, 0),
+                arrowprops=dict(facecolor='black', shrink=0.04, width=1),
+                )
+    ax.annotate('', xy=(2, 0), xytext=(3, 0),
+                arrowprops=dict(facecolor='black', shrink=0.04, width=1),
+                )
+
+    # r < 0
+    ax = fig.add_subplot(313)
+    frame1 = plt.gca()
+    r = -2
+    xdot = r*x - x**2
+    ax.plot(x,xdot,'b-')
+    ax.plot(0,0,'bo',fillstyle='full',mec='b')
+    ax.plot(r,0,'bo',fillstyle='right',mec='b')
+    ylim([min(xdot)-1,max(xdot)+3])
+    xlim([min(x),max(x)])
+    ax.plot(x,zeros_like(x),'k-')
+    ax.set_title(r'$r < 0$')
+    xlabel(r'$x$',fontsize=fs)
+    ylabel(r'$\dot{x}$',fontsize=fs)
+    frame1.axes.get_xaxis().set_visible(False)
+    frame1.axes.get_yaxis().set_visible(False)
+
+    ax.annotate('', xy=(r - 2, 0), xytext=(r-1, 0),
+                arrowprops=dict(facecolor='black', shrink=0.04, width=1),
+                )
+    ax.annotate('', xy=(-0.5, 0), xytext=(r+0.5, 0),
+                arrowprops=dict(facecolor='black', shrink=0.04, width=1),
+                )
+    ax.annotate('', xy=(-r-1, 0), xytext=(-r, 0),
+                arrowprops=dict(facecolor='black', shrink=0.04, width=1),
+                )
+                
+    # bifurcation diagram
+    x = arange(-5,5,0.1)
+    fig= figure(2)
+    ax = fig.add_subplot(111)
+    frame1 = plt.gca()
+    hold
+    rp = arange(0,5,0.1)
+    rn = arange(-5,0,0.1)
+    rzp = zeros_like(arange(0,5,0.1))
+    rzn = zeros_like(arange(-5,0,0.1))
+    ax.plot(arange(-5,0,0.1),rzn,'b-')
+    ax.plot(arange(0,5,0.1),rzp,'b--')
+    ax.plot(arange(-5,0,0.1),rn,'b--')
+    ax.plot(arange(0,5,0.1),rp,'b-')
+    axvline(x=0,color='k')
+    ax.set_title(r'$x^* = 0$')
+    frame1.axes.get_xaxis().set_visible(False)
+    frame1.axes.get_yaxis().set_visible(False)
+    
+    ## log example
+    
+    x = arange(-0.999, 5, 0.02)
+    fig = figure(3)
+    i = 1
+    my_xticks = ['' for j in x]
+    zidx = nonzero(x == min(abs(x)))[0][0]
+    my_xticks[zidx] = '0'
+    for r in [-2,1,2]:
+        ax = fig.add_subplot(310+i)
+        plot(r*x - log(1+x))
+        plot(zeros_like(x),'k-')
+        a = ax.get_xticks().tolist()
+        a[0:1] = ['',0]
+        a[2:] = ['' for k in a[2:]]
+        ax.set_xticklabels(a)
+        frame1 = plt.gca()
+        # frame1.axes.get_xaxis().set_visible(False)
+        frame1.axes.get_yaxis().set_visible(False)
+        i += 1
+    
+    fig = figure(4)
+    rn = arange(-0.999,0.01,0.1)
+    rp = arange(0.01,5,0.1)    
+    r = log(1+rp)/rp
+    rr = log(1+rn)/rn
+    ax = fig.add_subplot(111)
+    
+    plot(r,rp,'b--')
+    plot(rr,rn,'b-')
+    
+    plot(zeros(8),'k-')
+    a[0:1] = ['',1]
+    a[2:] = ['' for k in a[2:]]
+    ax.set_xticklabels(a)
+    frame1 = plt.gca()
+    # frame1.axes.get_xaxis().set_visible(False)
+    frame1.axes.get_yaxis().set_visible(False)
+    xlabel(r'$r$',fontsize=fs)
+    ylabel(r'$x^*$',fontsize=fs)
+    ax.set_title(r'$x^* = 0$')
+    
+    # linear approximation
+    fig = figure(5)    
+    ax = fig.add_subplot(111)
+    rn = arange(0,1,0.01)
+    rp = arange(1,2,0.1)
+    plot(rn,2-2*rn,'b--')
+    plot(rp,2-2*rp,'b')
+    plot(zeros(4),'k-')
+    plot(rn,zeros_like(rn),'b-')
+    a[0:1] = ['',1]
+    a[2:] = ['' for k in a[2:]]
+    ax.set_xticklabels(a)
+    frame1 = plt.gca()
+    # frame1.axes.get_xaxis().set_visible(False)
+    frame1.axes.get_yaxis().set_visible(False)
+    xlabel(r'$r$',fontsize=fs)
+    ax.set_title(r'$x^* = 0$')
+    
+    # both
+    fig = figure(6)    
+    ax = fig.add_subplot(111)
+    rn = arange(0,1,0.1)
+    rp = arange(1,2,0.1)
+    plot(zeros(8),'k-')
+    plot(rn,2-2*rn,'b--')
+    plot(rp,2-2*rp,'b')
+    rn = arange(-0.999,0.01,0.1)
+    rp = arange(0.01,2,0.1)
+    r = log(1+rp)/rp
+    rr = log(1+rn)/rn
+    plot(r,rp,'g--')
+    plot(rr,rn,'g-')
+    plot(rn,zeros_like(rn),'-')
+    a[0:1] = ['',1]
+    a[2:] = ['' for k in a[2:]]
+    ax.set_xticklabels(a)
+    frame1 = plt.gca()
+    # frame1.axes.get_xaxis().set_visible(False)
+    frame1.axes.get_yaxis().set_visible(False)
+    xlabel(r'$r$',fontsize=fs)
+    ax.set_title(r'$x^* = 0$')
+    
+    show()
+    
+    
